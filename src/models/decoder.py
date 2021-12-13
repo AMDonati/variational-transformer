correcting average ttn + input, attention and output latent in the VAE Transformer

src/models/decoder.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 from src.models.transformer_utils import positional_encoding, point_wise_feed_forward_network
-from src.models.self_attention import MultiHeadAttention
+from src.models.self_attention import MultiHeadAttention, PseudoSelfAttention
 
 
 class DecoderLayer(tf.keras.layers.Layer):
@@ -77,10 +77,13 @@ class Decoder(tf.keras.layers.Layer):
 
 
 class VAEDecoderLayer(tf.keras.layers.Layer):
-    def __init__(self, d_model, num_heads, dff, rate=0.1):
+    def __init__(self, d_model, num_heads, dff, rate=0.1, latent="input"):
         super(VAEDecoderLayer, self).__init__()
 
-        self.mha = MultiHeadAttention(d_model, num_heads)
+        if latent == "attention":
+            self.mha = PseudoSelfAttention(d_model, num_heads)
+        else:
+            self.mha = MultiHeadAttention(d_model, num_heads)
         self.ffn = point_wise_feed_forward_network(d_model, dff)
 
         self.layernorm1 = tf.keras.layers.LayerNormalization(epsilon=1e-6)
@@ -89,8 +92,8 @@ class VAEDecoderLayer(tf.keras.layers.Layer):
         self.dropout1 = tf.keras.layers.Dropout(rate)
         self.dropout2 = tf.keras.layers.Dropout(rate)
 
-    def call(self, query, keys, values, training, mask):
-        attn_output, attention_weights_block = self.mha(q=query, k=keys, v=values,  mask=mask)  # (batch_size, input_seq_len, d_model)
+    def call(self, query, keys, values, training, mask, z=None):
+        attn_output, attention_weights_block = self.mha(q=query, k=keys, v=values,  mask=mask, z=z)  # (batch_size, input_seq_len, d_model)
         attn_output = self.dropout1(attn_output, training=training)
         out1 = self.layernorm1(query + attn_output)  # (batch_size, input_seq_len, d_model)
 
@@ -106,9 +109,13 @@ class VAEDecoder(Decoder):
         super(VAEDecoder, self).__init__(num_layers=num_layers, d_model=d_model, num_heads=num_heads, dff=dff, target_vocab_size=target_vocab_size,
                  maximum_position_encoding=maximum_position_encoding, rate=0.1)
 
-        self.dec_layers = [VAEDecoderLayer(d_model, num_heads, dff, rate)
+        self.dec_layers = [VAEDecoderLayer(d_model, num_heads, dff, rate, latent=latent)
                            for _ in range(num_layers)]
         self.latent = latent
+        if self.latent == "input":
+            self.input_proj = tf.keras.layers.Dense(d_model, use_bias=False)
+        if self.latent == "attention":
+            self.attn_proj = tf.keras.layers.Dense(d_model*num_layers, use_bias=False)
 
     def pseudo_self_attention(self, x, z):
         # x: shape (batch_size, tar_seq_len, d_model)
@@ -127,20 +134,20 @@ class VAEDecoder(Decoder):
         x += self.pos_encoding[:, :seq_len, :]
 
         if self.latent == "input":
-            x = x + z
+            x = x + self.input_proj(z)
 
         x = self.dropout(x, training=training)
 
+        if self.latent == "attention":
+            z = tf.split(self.attn_proj(z), num_or_size_splits=self.num_layers, axis=-1)
 
         for i in range(self.num_layers):
             if self.latent == "attention":
-                keys = self.pseudo_self_attention(x, z) # shape (B, S+1, d_model)
-                values = self.pseudo_self_attention(x, z) # shape (B, S+1, d_model)
-            else:
-                keys = x
-                values = x
+                z_i = z[i]
+                x, block = self.dec_layers[i](query=x, keys=x, values=x, z=z_i, training=training, mask=look_ahead_mask)
 
-            x, block = self.dec_layers[i](query=x, keys=keys, values=values, training=training, mask=look_ahead_mask)
+            else:
+                x, block = self.dec_layers[i](query=x, keys=x, values=x, training=training, mask=look_ahead_mask)
 
             attention_weights['decoder_layer{}'.format(i + 1)] = block
 

src/models/encoder.py
@@ -68,8 +68,9 @@ class VAEEncoder(Encoder):
         super(VAEEncoder, self).__init__(num_layers=num_layers, d_model=d_model, num_heads=num_heads, dff=dff,
                                          input_vocab_size=input_vocab_size,
                                          maximum_position_encoding=maximum_position_encoding, rate=rate)
-        self.average_attention = MultiHeadAttention(d_model, num_heads)
+        self.average_attention = MultiHeadAttention(d_model, num_heads=1, non_linearity=True, scale=False) # in previous work, has a gelu non linearity.
         self.learnable_query = tf.Variable(initial_value=tf.random.normal(shape=(1, 1, d_model)), name="learnable_query")
+        #nn.init.normal_(w, std=0.02): in Transformer VAE: init with a random normal.
 
     def call(self, x, training, mask):
         seq_len = tf.shape(x)[1]

src/models/self_attention.py
@@ -1,6 +1,6 @@
 import tensorflow as tf
 
-def scaled_dot_product_attention(q, k, v, mask):
+def scaled_dot_product_attention(q, k, v, mask, non_linearity=False, scale=True):
     """Calculate the attention weights.
     q, k, v must have matching leading dimensions.
     k, v must have matching penultimate dimension, i.e.: seq_len_k = seq_len_v.
@@ -20,9 +20,15 @@ def scaled_dot_product_attention(q, k, v, mask):
 
     matmul_qk = tf.matmul(q, k, transpose_b=True)  # (..., seq_len_q, seq_len_k)
 
-    # scale matmul_qk
-    dk = tf.cast(tf.shape(k)[-1], tf.float32)
-    scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+    if scale:
+        # scale matmul_qk
+        dk = tf.cast(tf.shape(k)[-1], tf.float32)
+        scaled_attention_logits = matmul_qk / tf.math.sqrt(dk)
+    else:
+        scaled_attention_logits = matmul_qk
+
+    if non_linearity:
+        scaled_attention_logits = tf.keras.activations.gelu(scaled_attention_logits)
 
     # add the mask to the scaled tensor.
     if mask is not None:
@@ -38,7 +44,7 @@ def scaled_dot_product_attention(q, k, v, mask):
 
 
 class MultiHeadAttention(tf.keras.layers.Layer):
-    def __init__(self, d_model, num_heads):
+    def __init__(self, d_model, num_heads, non_linearity=False, scale=True):
         super(MultiHeadAttention, self).__init__()
         self.num_heads = num_heads
         self.d_model = d_model
@@ -53,6 +59,9 @@ class MultiHeadAttention(tf.keras.layers.Layer):
 
         self.dense = tf.keras.layers.Dense(d_model)
 
+        self.non_linearity = non_linearity # for average attention: use a gelu activation
+        self.scale = scale # for average attention: no scaling.
+
     def split_heads(self, x, batch_size):
         """Split the last dimension into (num_heads, depth).
         Transpose the result such that the shape is (batch_size, num_heads, seq_len, depth)
@@ -60,7 +69,7 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         x = tf.reshape(x, (batch_size, -1, self.num_heads, self.depth))
         return tf.transpose(x, perm=[0, 2, 1, 3])
 
-    def call(self, q, k, v, mask):
+    def call(self, q, k, v, mask, z=None):
         batch_size = tf.shape(q)[0]
 
         q = self.wq(q)  # (batch_size, seq_len, d_model)
@@ -74,7 +83,47 @@ class MultiHeadAttention(tf.keras.layers.Layer):
         # scaled_attention.shape == (batch_size, num_heads, seq_len_q, depth)
         # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
         scaled_attention, attention_weights = scaled_dot_product_attention(
-            q, k, v, mask)
+            q, k, v, mask, non_linearity=self.non_linearity, scale=self.scale)
+
+        scaled_attention = tf.transpose(scaled_attention,
+                                        perm=[0, 2, 1, 3])  # (batch_size, seq_len_q, num_heads, depth)
+
+        concat_attention = tf.reshape(scaled_attention,
+                                      (batch_size, -1, self.d_model))  # (batch_size, seq_len_q, d_model)
+
+        output = self.dense(concat_attention)  # (batch_size, seq_len_q, d_model)
+
+        return output, attention_weights
+
+class PseudoSelfAttention(MultiHeadAttention):
+    def __init__(self, d_model, num_heads, non_linearity=False, scale=True):
+        super(PseudoSelfAttention, self).__init__(d_model=d_model, num_heads=num_heads, non_linearity=non_linearity, scale=scale)
+
+        self.wz = tf.keras.layers.Dense(2*d_model)
+
+    def call(self, q, k, v, z, mask):
+        batch_size = tf.shape(q)[0]
+
+        q = self.wq(q)  # (batch_size, seq_len, d_model)
+        k = self.wk(k)  # (batch_size, seq_len, d_model)
+        v = self.wv(v)  # (batch_size, seq_len, d_model)
+
+        q = self.split_heads(q, batch_size)  # (batch_size, num_heads, seq_len_q, depth)
+        k = self.split_heads(k, batch_size)  # (batch_size, num_heads, seq_len_k, depth)
+        v = self.split_heads(v, batch_size)  # (batch_size, num_heads, seq_len_v, depth)
+
+        # adding key_z and value_z from z:
+        key_z, value_z = tf.split(self.wz(z), num_or_size_splits=2, axis=-1)
+        key_z = self.split_heads(key_z, batch_size)
+        value_z = self.split_heads(value_z, batch_size)
+
+        k = tf.concat([key_z, k], axis=-2)
+        v = tf.concat([value_z, v], axis=-2)
+
+        # scaled_attention.shape == (batch_size, num_heads, seq_len_q, depth)
+        # attention_weights.shape == (batch_size, num_heads, seq_len_q, seq_len_k)
+        scaled_attention, attention_weights = scaled_dot_product_attention(
+            q, k, v, mask, non_linearity=self.non_linearity, scale=self.scale)
 
         scaled_attention = tf.transpose(scaled_attention,
                                         perm=[0, 2, 1, 3])  # (batch_size, seq_len_q, num_heads, depth)

src/models/transformer.py
@@ -1,8 +1,7 @@
 import tensorflow as tf
 from src.models.encoder import Encoder, VAEEncoder
 from src.models.decoder import Decoder, VAEDecoder
-from src.models.transformer_utils import create_look_ahead_mask, create_padding_mask
-from src.models.vae_utils import gaussian_kld
+from src.models.transformer_utils import create_look_ahead_mask, create_padding_mask, point_wise_feed_forward_network
 
 
 class Transformer(tf.keras.Model):
@@ -33,7 +32,7 @@ class Transformer(tf.keras.Model):
 
         return final_output, attention_weights, 0.  # 0. is for KL loss.
 
-    def create_masks(self, inp, tar):
+    def create_masks(self, inp, tar, size1=None, size2=None):
         # Encoder padding mask
         enc_padding_mask = create_padding_mask(inp)
 
@@ -44,7 +43,11 @@ class Transformer(tf.keras.Model):
         # Used in the 1st attention block in the decoder.
         # It is used to pad and mask future tokens in the input received by
         # the decoder.
-        look_ahead_mask = create_look_ahead_mask(tf.shape(tar)[1])
+        if size1 is None or size2 is None:
+            look_ahead_mask = create_look_ahead_mask(tf.shape(tar)[1], tf.shape(tar)[1])
+        else:
+            look_ahead_mask = create_look_ahead_mask(size1, size2)
+
         dec_target_padding_mask = create_padding_mask(tar)
         look_ahead_mask = tf.maximum(dec_target_padding_mask, look_ahead_mask)
 
@@ -69,6 +72,13 @@ class VAETransformer(Transformer):
         self.posterior_net = tf.keras.layers.Dense(2 * d_model, name='posterior_net')
         # self.combination_layer = tf.keras.layers.Dense(d_model)
 
+        if self.decoder.latent == "output":
+            self.output_proj = tf.keras.Sequential([
+                tf.keras.layers.Dense(dff),  # (batch_size, seq_len, dff)
+                tf.keras.layers.LeakyReLU(),
+                tf.keras.layers.Dense(target_vocab_size)  # (batch_size, seq_len, d_model)
+            ])
+
     def encode_prior(self, x):
         mean, logvar = tf.split(self.prior_net(x), num_or_size_splits=2, axis=-1)
         return mean, logvar
@@ -81,12 +91,12 @@ class VAETransformer(Transformer):
         eps = tf.random.normal(shape=mean.shape)
         return eps * tf.exp(logvar * .5) + mean
 
-    def combination_layer(self, x, z):
-        # x: shape of (batch_size, tar_seq_len, d_model)
-        # z: shape of (batch size, d_model)
-        out_ = self.final_layer(x)
-        p_z = self.final_layer(z)
-        return out_ + p_z
+    # def combination_layer(self, x, z):
+    #     # x: shape of (batch_size, tar_seq_len, d_model)
+    #     # z: shape of (batch size, d_model)
+    #     out_ = self.final_layer(x)
+    #     p_z = self.final_layer(z)
+    #     return out_ + p_z
 
     def compute_kl(self, prior_mean, prior_logvar, recog_mean, recog_logvar):
         kld = -0.5 * tf.math.reduce_sum(1 + (recog_logvar - prior_logvar)
@@ -101,8 +111,17 @@ class VAETransformer(Transformer):
             encoder_input = tf.concat([inp, tar], axis=1)
         else:
             encoder_input = inp
-        enc_padding_mask, look_ahead_mask, dec_padding_mask = self.create_masks(encoder_input,
-                                                                                tar)  # TODO: problem here for latent = "attention". The decoding padding mask should include one more timestep for the latent.
+
+        if self.decoder.latent == "attention":
+            # add a dummy timestep to tar to create masks with the right length for pseudo self-attention:
+            tar_ = tf.concat([tf.ones_like(tf.expand_dims(tar[:, 0], axis=1)), tar], axis=1)
+            enc_padding_mask, look_ahead_mask, dec_padding_mask = self.create_masks(encoder_input,
+                                                                                    tar_, size1=tar.shape[1],
+                                                                                    size2=tar_.shape[1])
+        else:
+            tar_ = tar
+            enc_padding_mask, look_ahead_mask, dec_padding_mask = self.create_masks(encoder_input,
+                                                                                    tar_)  # TODO: problem here for latent = "attention". The decoding padding mask should include one more timestep for the latent.
 
         enc_output = self.encoder(encoder_input, training,
                                   enc_padding_mask)  # (batch_size, inp_seq_len, d_model) #TODO: do we need an enc_padding mask ?
@@ -122,7 +141,8 @@ class VAETransformer(Transformer):
             tar, z, training, look_ahead_mask)
 
         if self.decoder.latent == "output":
-            final_output = self.combination_layer(dec_output, z)
+            z = self.output_proj(z)
+            final_output = self.final_layer(dec_output) + z
         else:
             final_output = self.final_layer(dec_output)  # (batch_size, tar_seq_len, target_vocab_size)
 

src/models/transformer_utils.py
@@ -28,8 +28,8 @@ def create_padding_mask(seq):
     # to the attention logits.
     return seq[:, tf.newaxis, tf.newaxis, :]  # (batch_size, 1, 1, seq_len)
 
-def create_look_ahead_mask(size):
-    mask = 1 - tf.linalg.band_part(tf.ones((size, size)), -1, 0)
+def create_look_ahead_mask(size1, size2):
+    mask = 1 - tf.linalg.band_part(tf.ones((size1, size2)), -1, 0)
     return mask  # (seq_len, seq_len)
 
 def create_masks(inp, tar):
@@ -49,11 +49,13 @@ def create_masks(inp, tar):
 
     return enc_padding_mask, combined_mask, dec_padding_mask
 
+
 def point_wise_feed_forward_network(d_model, dff):
-  return tf.keras.Sequential([
-      tf.keras.layers.Dense(dff, activation='relu'),  # (batch_size, seq_len, dff)
-      tf.keras.layers.Dense(d_model)  # (batch_size, seq_len, d_model)
-  ])
+    return tf.keras.Sequential([
+        tf.keras.layers.Dense(dff, activation='relu'),  # (batch_size, seq_len, dff)
+        tf.keras.layers.Dense(d_model)  # (batch_size, seq_len, d_model)
+    ])
+
 
 
 
@@ -66,3 +68,7 @@ if __name__ == "__main__":
     d_model = 64
     inputs = tf.random.uniform(shape=(b, S, d_model))
     pos_enc = positional_encoding(position=pe_target, d_model=d_model)
+
+    # test of look_ahead mask:
+    mask = create_look_ahead_mask(36, 37)
+    print("done")